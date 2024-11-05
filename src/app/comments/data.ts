f8901a230c5f72d3aa31b4fbb1ff77e2a603fdc0
chebon254 <kelvinchebon90@gmail.com>
2024-11-05 21:29:35 +0300

    Route handling

---
+++ src/app/comments/data.ts
@@ -0,0 +1,18 @@
+export const comments = [
+    {
+        id: 1,
+        text: "This is comment one"
+    },
+    {
+        id: 2,
+        text: "This is comment two"
+    },
+    {
+        id: 3,
+        text: "This is comment three"
+    },
+    {
+        id: 4,
+        text: "This is comment four"
+    }
+];
